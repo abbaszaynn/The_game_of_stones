c520c89b932d077cff583ec6d3f1064d766e2f66
ABBAS ZAYN <abbaszayn08@gmail.com>
2025-09-25 19:02:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7834733067-6b6d8",
+  "appId": "1:122428655785:web:0e7a74d078829b269181b8",
+  "apiKey": "AIzaSyDs62QB4ODdJ_ZAMGcoxszWm8nW_EZA9pE",
+  "authDomain": "studio-7834733067-6b6d8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "122428655785"
+};
