8f38a6de1ebfe735385ddf8b609b7b3f00151f50
ABBAS ZAYN <abbaszayn08@gmail.com>
2025-09-26 16:58:39 +0000

    ADD THIS GEOLOGICAL REPORT AS WELL and name as Marble Ore at Bagicha. :

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -6,6 +6,7 @@ const findImage = (id: string) => PlaceHolderImages.find((img) => img.id === id)
 
 const documents: Document[] = [
   { id: 'doc-1', companyId: 'durr-mines-and-minerals', title: 'Geological Field Report of Marble Ore at Bagicha', type: 'Geological Report', url: 'https://mega.nz/file/gIZRxJZY#iumoVJkKeejUdux1fQz-Y31o0lAMDcMhJ3X-KnDNyL0' },
+  { id: 'doc-7', companyId: 'durr-mines-and-minerals', title: 'Marble Ore at Bagicha', type: 'Geological Report', url: 'https://mega.nz/file/4UhxmTKY#AzFv7Jc3IE11OhBKdGSbaRYKM85UhmtwrOKc96f9vcI' },
   { id: 'doc-2', companyId: 'durr-mines-and-minerals', title: 'MINING LICENSE', type: 'License', url: '#' },
   { id: 'doc-3', companyId: 'earth-lux-mines', title: 'Jutial Nala Prospectus', type: 'Geological Report', url: '#' },
   { id: 'doc-4', companyId: 'earth-lux-mines', title: 'Annual Financial Summary 2023', type: 'Financial Summary', url: '#' },
