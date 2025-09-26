8b756a997179304529360c31785a5f923aa75de0
ABBAS ZAYN <abbaszayn08@gmail.com>
2025-09-26 16:20:06 +0000

    make the aspect ratio of image div 16:10 (_for element img_)

--- src/app/companies/[id]/page.tsx
+++ src/app/companies/[id]/page.tsx
@@ -172,8 +172,8 @@ export default async function CompanyDetailPage({ params }: { params: { id: stri
                     <CarouselItem key={index}>
                       <div className="p-1">
                         <Card>
-                          <CardContent className="flex aspect-[21/9] items-center justify-center p-0">
-                            <Image src={src} alt={`Gallery image ${index + 1}`} width={1280} height={548} className="rounded-lg object-cover" data-ai-hint="mining site" />
+                          <CardContent className="flex aspect-[16/10] items-center justify-center p-0">
+                            <Image src={src} alt={`Gallery image ${index + 1}`} width={1280} height={800} className="rounded-lg object-cover" data-ai-hint="mining site" />
                           </CardContent>
                         </Card>
                       </div>
@@ -272,3 +272,5 @@ export default async function CompanyDetailPage({ params }: { params: { id: stri
     </div>
   );
 }
+
+    
