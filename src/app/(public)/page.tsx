1c4dfdb260c27881ce68bd8f023b367bc9123011
ansaralyh <abbaszayn827@gmail.com>
2026-02-06 21:25:33 +0500

    cover image generated

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -77,7 +77,7 @@ export default async function Home() {
             {/* Background Image */}
             <div className="absolute inset-0">
               <NextImage
-                src="/images/gallery-background.png"
+                src="/images/cover_photo.png"
                 alt="Mining exploration site gallery background"
                 fill
                 className="object-cover transition-transform duration-1000 group-hover:scale-105"
